8c63ae667c0b6b52ffbeec9e5ccbfdd6a7401178
BranY <yangwen_jia@163.com>
2016-07-25 14:07:42 +0800

    2016-7-25

---
+++ Algorithm/.idea/fileTemplates/includes/C File Header.h
@@ -0,0 +1,10 @@
+#if ($HEADER_COMMENTS)
+//
+// Created by BranY on ${DATE}.
+// Contact at wyang@smail.nju.edu.cn
+#if ($ORGANIZATION_NAME && $ORGANIZATION_NAME != "")
+// Copyright (c) $YEAR ${ORGANIZATION_NAME}#if (!$ORGANIZATION_NAME.endsWith(".")).#end All rights reserved.
+#end
+//
+#end
+
