f663b3da5e2fb7bf1b9926e552e69c420cfed9f8
BranY <yangwen_jia@163.com>
2016-08-31 20:16:08 +0800

    "2016-8-31"

--- Algorithm/AA.cpp
+++ Algorithm/AA.cpp
@@ -5,6 +5,7 @@
 //
 
 #include <iostream>
+#include <climits>
 
 using namespace std;
 class A {
@@ -23,6 +24,26 @@ class D: public B, public C {
 public:
     int sum;
 };
+int myAtoi(string str) {
+    if (str.size() == 0) return 0;
+    int i = 0, res = 0;
+    while (i < str.size() && str[i] == ' ') i++;
+
+    int sign = (str[i] == '-') ? -1 : 1;
+    if (str[i] == '-' || str[i] == '+') i++;
+    while (i < str.size()) {
+        if (!isdigit(str[i])) break;
+        int digit = str[i] - '0';
+        cout << digit << "----" << endl;
+        if (sign == -1 && res > -((INT_MIN + digit) / 10))
+            return INT_MIN;
+        else if (sign == 1 && res > ((INT_MAX -digit) / 10))
+            return INT_MAX;
+        res = res * 10 + digit;
+        i++;
+    }
+    return sign * res;
+}
 int main() {
     D d;
     d.i = 1;
@@ -30,5 +51,9 @@ int main() {
     d.k = 3;
     d.sum = d.i + d.j + d.k;
     cout << d.sum << endl;
+    string str;
+    while (getline(cin, str)) {
+        cout << myAtoi(str) << endl;
+    }
     return 0;
 }
