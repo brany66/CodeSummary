0a451a7abc3457705a77ebf145e022d620b7b209
BranY <yangwen_jia@163.com>
2016-09-15 16:36:58 +0800

    "2016-9-15"

---
+++ Algorithm/BaiDu/test.cpp
@@ -0,0 +1,28 @@
+//
+// Created by BranY on 2016/9/13.
+// Contact at wyang@smail.nju.edu.cn
+// Copyright (c) 2016 NJU PASA Lab All rights reserved.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+
+
+using namespace std;
+
+int main() {
+    //map<char, int> index;
+    string stringA;
+    vector<int> index(256, -1);
+
+    for (int i = 0; i < stringA.size(); i++) {
+    }
+    return 0;
+}
